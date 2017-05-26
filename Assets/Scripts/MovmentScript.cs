0f6ace76b07d2af7d1e1a799ef62d257bd462a94
Marcos de Andrade <Marcos de Andrade>
2017-05-26 20:39:29 -0300

    Adicionando novas sprites e mudancas feitas em alguns dos scripts

---
+++ Assets/Scripts/MovmentScript.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MovmentScript : MonoBehaviour {
+
+	private Rigidbody2D myRigidbody2D;
+    public Animator animacao;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundDistance;
+
+	public int speed;
+	public float jumpforce;
+    public float maxJumpSpeed;
+	
+	private bool down;
+    private bool grounded; 
+	
+
+	void Start () {
+
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+		animacao = GetComponent<Animator>(); 
+
+		down = false;					
+        jumpforce = 8;
+        speed = 8;
+        maxJumpSpeed = 2;
+    }
+
+
+    //-----------------------------------------------------------------------//
+
+
+    private void Update() {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundDistance, whatIsGround);
+        if (grounded) speed = 8;
+        else speed = 2;
+
+        if (Input.GetKey(KeyCode.D) && !down){
+            if (grounded){
+                myRigidbody2D.AddForce(new Vector2(speed, 0) - myRigidbody2D.velocity);
+            }
+            else if (myRigidbody2D.velocity.x < maxJumpSpeed) myRigidbody2D.AddForce(new Vector2(speed, 0));
+        }
+
+
+        //-----------------------------------------------------------------------//
+
+        if (Input.GetKey(KeyCode.A) && !down){
+            if (grounded){
+                myRigidbody2D.AddForce(new Vector2(-speed, 0) - myRigidbody2D.velocity);
+            }
+            else if (myRigidbody2D.velocity.x > -maxJumpSpeed) myRigidbody2D.AddForce(new Vector2(-speed, 0));
+        }
+
+        //-----------------------------------------------------------------------//
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+            myRigidbody2D.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+
+
+        //-----------------------------------------------------------------------//
+
+        if (Input.GetKey(KeyCode.S))
+            down = true;
+
+
+        //-----------------------------------------------------------------------//
+
+
+        if (Input.GetKeyUp(KeyCode.S))
+            down = false;
+
+
+        //-----------------------------------------------------------------------//
+
+
+        /**if (Input.GetKey("p")){ 
+			myRigidbody2D.velocity = Vector2.zero;
+		}**/
+
+
+        //-----------------------------------------------------------------------//
+        
+
+        if (myRigidbody2D.velocity.x < 0)
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        else if (myRigidbody2D.velocity.x > 0)
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+
+        //-----------------------------------------------------------------------//
+
+
+        animacao.SetFloat("Speed", Mathf.Abs(myRigidbody2D.velocity.x));
+        animacao.SetBool("Down", down);
+        animacao.SetBool("Jump", !grounded);
+    }
+}	
---
+++ Assets/Scripts/PadController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadController : MonoBehaviour {
+
+    public bool isActive = false;
+    public bool isVisible = false;
+    public Vector2 velocity;
+    private Rigidbody2D rb;
+
+    private void FixedUpdate() {
+
+    }
+
+    void OnTriggerStay2D(Collider2D objeto) {
+
+        rb = objeto.GetComponent<Rigidbody2D>();
+
+        if (rb.CompareTag("Player")) {
+
+            rb.AddForce(velocity * Time.deltaTime, ForceMode2D.Impulse);
+        }
+    }
+}
