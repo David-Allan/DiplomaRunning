bfb01102490f8bde8796ede3bb2b8bcba1c614fb
Marcos de Andrade <marcosscamor@hotmail.com>
2017-05-30 18:54:22 -0300

    Som de coleta das moedas funcionando (pode ser usado como base para outros) e pequenas mudanças

--- Assets/CollectScript.cs
+++ Assets/CollectScript.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-//[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(AudioSource))]
 
 public class CollectScript : MonoBehaviour {
+
+    public AudioClip collect;
 
     private void OnTriggerEnter2D(Collider2D collider) {
 
         if (collider.CompareTag("Player")) {
 
-            gameObject.GetComponent<AudioSource>().Play();
             collider.GetComponent<MovementScript>().coins++;
+            AudioSource.PlayClipAtPoint(collect, transform.position);
             gameObject.SetActive(false);
         }
     }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    static int totalCoins;
+    
+	void Start () {
+
+        totalCoins = 0;
+	}
+}
