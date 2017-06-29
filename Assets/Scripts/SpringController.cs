977609715bbef27ebe334adb52e10e183fbc7703
david-allan <davidallancordeiro@hotmail.com>
2017-06-29 02:08:16 -0300

    incluido adversarios animados

---
+++ Assets/Scripts/SpringController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringController : MonoBehaviour {
+
+    public Vector2 velocidade;
+    public AudioClip som;
+	public Animator animacao;
+
+
+	void Start(){
+		animacao = GetComponent<Animator>();
+	}
+
+    void OnTriggerEnter2D(Collider2D objeto) {
+		
+        if (objeto.CompareTag("Player")) {
+
+            AudioSource.PlayClipAtPoint(som, transform.position);
+            objeto.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            objeto.GetComponent<Rigidbody2D>().AddForce(velocidade, ForceMode2D.Impulse);
+			animacao.SetTrigger("gatilho");
+
+        }
+
+    }
+}
